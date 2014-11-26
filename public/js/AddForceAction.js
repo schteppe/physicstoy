a7310275ccaa4a0214afabdb72e21b104e7f043a
Stefan Hedman <schteppe@gmail.com>
2014-11-26 08:16:16 +0100

    Update AddForceAction.js

--- public/js/AddForceAction.js
+++ public/js/AddForceAction.js
@@ -1,1 +1,14 @@
-Aa
+function AddForceAction(options){
+	Action.apply(this, arguments);
+	options = options || {};
+
+	this.force = options.force ? options.force.slice(0) : [0, 0];
+	this.angularForce = options.angularForce || 0;
+}
+AddForceAction.prototype = Object.create(Action.prototype);
+AddForceAction.prototype.enter = function(machine){
+	machine.parent.force.set(this.force);
+	machine.parent.angularForce = this.angularForce;
+};
+AddForceAction.prototype.update = function(){};
+AddForceAction.prototype.exit = function(){};
