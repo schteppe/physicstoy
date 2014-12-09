fb6c5ba8ebd8befee3926374cf4db5a28f2f000c
Stefan Hedman <schteppe@gmail.com>
2014-12-09 20:52:47 +0100

    better browsing

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,24 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
 	// Init thumbnails
-	var canvases = document.getElementsByClassName('thumbnail');
-	for (var i = 0; i < canvases.length; i++) {
-		var canvas = canvases[i];
+	var containers = document.getElementsByClassName('thumbnail');
+	var dpr = window.devicePixelRatio || 1;
+	for (var i = 0; i < containers.length; i++) {
+		var container = containers[i];
+
+		var canvas = document.createElement('CANVAS');
+		canvas.width = 100 * dpr;
+		canvas.height = 100 * dpr;
+		container.appendChild(canvas);
 		var ctx = canvas.getContext('2d');
-		var children = canvas.childNodes;
+
+		var children = container.childNodes;
 		var scriptNode;
 		for(var j=0; j<children.length; j++){
 			if(children[j].type === 'application/json')
 				scriptNode = children[j];
 		}
+
 		try {
 			var scene = JSON.parse(scriptNode.innerHTML);
 			Thumbnail.drawOnCanvas(ctx, scene, canvas.width, canvas.height, {});
